801a8f76b4002f37d6879cadd25f13b0bd1d27a7
Muhammad Taufik <MKM02120@ptmkm.co.id>
2024-11-01 11:47:20 +0700

    done

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -21,7 +21,7 @@
 
 <main>
     <header class="page-header page-header-dark bg-gradient-primary-to-secondary pb-10">
-        <div class="container-xl px-4">
+        <div class="container-fluid px-4">
             <div class="page-header-content pt-4">
                 <div class="row align-items-center justify-content-between">
                     <div class="col-auto mt-2">
